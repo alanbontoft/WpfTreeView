bb00f86b4f91dfc2e7815b897fdff64a6ee99f82
Alan Bontoft <alan.bontoft@hotmail.co.uk>
2019-04-25 21:53:26 +0100

    images added.  Code started

--- WpfTreeView/MainWindow.xaml.cs
+++ WpfTreeView/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace WpfTreeView
 {
@@ -20,9 +21,81 @@ namespace WpfTreeView
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// When main window opens
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach(var drive in Directory.GetLogicalDrives())
+            {
+                var item = new TreeViewItem()
+                {
+                    Header = drive,
+                    Tag = drive
+                };
+
+                // Add dummy item to get expand icon
+                item.Items.Add(null);
+
+                // Listen for folder expanded
+                item.Expanded += Folder_Expanded;
+
+                // Add item to main treeview
+                FolderView.Items.Add(item);
+            }
+        }
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            // return if item already processed
+            if (item.Items.Count != 1 || item.Items[0] != null) return;
+
+            // clear dummy item
+            item.Items.Clear();
+
+            var fullPath = (string)item.Tag;
+
+            var directories = new List<string>();
+
+            try
+            {
+                var dirs = Directory.GetDirectories(fullPath);
+
+                if (dirs.Length > 0)
+                    directories.AddRange(dirs);
+            }
+            catch {  }
+
+            // for each directory in list
+            directories.ForEach(directoryPath =>
+            {
+                var subItem = new TreeViewItem()
+                {
+                    Header = directoryPath,
+                    Tag = directoryPath
+                };
+
+                subItem.Items.Add(null);
+
+                subItem.Expanded += Folder_Expanded;
+
+                item.Items.Add(subItem);
+            });
+
+
+
         }
     }
 }
